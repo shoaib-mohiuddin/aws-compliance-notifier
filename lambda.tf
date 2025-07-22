bd64999ed529c59a5c22557454b6967f7e72cf7f
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2025-07-22 17:20:25 +0530

    Adds environment variables, eventbridge rule and trigger

--- lambda.tf
+++ lambda.tf
@@ -1,10 +1,18 @@
+data "aws_ses_email_identity" "ses1" {
+  email = "shoaib.mohiuddin@cloudreach.com"
+}
+
+data "aws_ses_email_identity" "ses2" {
+  email = "shoaibmm7@gmail.com"
+}
+
 data "archive_file" "python_script_file" {
   type        = "zip"
-  source_file = "${path.module}/python/*"
+  source_dir  = "${path.module}/python/"
   output_path = "${path.module}/files/lambda-function.zip"
 }
 
-resource "aws_lambda_function" "lab_lambda_image_rekognition" {
+resource "aws_lambda_function" "audit_lambda" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
   filename      = data.archive_file.python_script_file.output_path
@@ -16,5 +24,34 @@ resource "aws_lambda_function" "lab_lambda_image_rekognition" {
 
   runtime = "python3.11"
   timeout = 10
+
+  environment {
+    variables = {
+      SES_SENDER = data.aws_ses_email_identity.ses1.email
+      SES_RECIPIENT  = data.aws_ses_email_identity.ses2.email
+    }
+  }
+
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_event" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.audit_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_event_rule.arn
+}
+
+resource "aws_cloudwatch_event_rule" "lambda_event_rule" {
+  name        = "${var.lambda_function_name}_event_rule"
+  description = "Trigger for Lambda function to audit EBS volumes"
+
+  schedule_expression = "cron(0 12 * * ? *)" # Runs every day at 12:00 UTC
+  state               = "ENABLED"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule = aws_cloudwatch_event_rule.lambda_event_rule.name
+  arn  = aws_lambda_function.audit_lambda.arn
 
 }
